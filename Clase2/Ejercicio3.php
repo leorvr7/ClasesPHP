ebda6988fbd0263f002e8292c9f008355de78457
Leonardo Villar Rodriguez <leorvr7@gmail.com>
2018-09-18 19:54:05 -0500

    nuevo ingreso

--- Clase2/Ejercicio3.php
+++ Clase2/Ejercicio3.php
@@ -1,27 +1,22 @@
 <!doctype html>
 <html lang="es">
 <head>
-	<meta charset="}">
+	<meta charset="UTF-8">
 	<meta name="viewport"
 	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible"
 	      content="ie=edge">
-	<link rel="stylesheet" href="estilos2.css">
 	<title>Document</title>
 </head>
 <body>
 <?php
-include 'menu.php';
-$menu = new Menu();
-
-print "<ul class='menu'>";
-print "
-{$menu->registrar()}
-{$menu->insertar()}
-{$menu->obtener()}
-{$menu->modificar()}
-{$menu->eliminar()}";
-print "</ul>";
+include 'Factura.php';
+$factura1 = new Factura(5000, 'Pagado');
+$factura2 = new Factura(7500, 'Deuda');
+$factura3=new Factura(15690,'Pagado');
+$factura1->imprimir();
+$factura2->imprimir();
+$factura3->imprimir();
 ?>
 </body>
 </html>
