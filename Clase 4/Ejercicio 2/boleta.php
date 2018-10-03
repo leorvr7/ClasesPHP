0ce12c90a9d55286c12f0cc80c7c98e24361c0ce
Leonardo Villar Rodriguez <leorvr7@gmail.com>
2018-10-03 13:16:57 -0500

    rufian

---
+++ Clase 4/Ejercicio 2/boleta.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace principal;
+
+
+class boleta
+{
+	public function __construct($codigo, $Apellidop, $apellidom)
+	{
+
+	}
+}
+
+class datos_personales
+{
+
+}
---
+++ Clase 4/Ejercicio 2/index.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible"
+	      content="ie=edge">
+	<title>Ejercicio 2</title>
+	<link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+
+<h1 class="Titulo2">Ejercicio 2</h1>
+<article class="boleta">
+	<nav class="Navegador">
+		<article class="parte">
+			<img src="descarga.png"
+			     class="img-logo">
+		</article>
+		<article class="parte">
+			<p>PERUANA DE COMBUSTIBLES S.A.</p>
+			<p>RUC: 20259033072</p>
+			<p>Calle Coronel Andrés N°360 Lima</p>
+		</article>
+	</nav>
+	<section class="bol-fec">
+		<p>Boleta de Pago</p>
+		<p>Del 01.09.2016 al 30.09.2016</p>
+	</section>
+	<section class="cuerpo-bol">
+		<section class="cabeza-boleta">
+			<div class="div-cab">
+				<p>Código</p>
+			</div>
+			<div class="div-cab">
+				<p>Apellidos y Nombre</p>
+			</div>
+			<div class="div-cab">
+				<p>Fecha Ing.</p>
+			</div>
+			<div class="div-cab">
+				<p>Fecha Cese</p>
+			</div>
+			<div class="div-cab">
+				<p>Sueldo</p>
+			</div>
+		</section>
+	</section>
+</article>
+</body>
+</html>
